55da19de26ceb7d39391e4b5c20b162a83bd9fd9
Olufemi Oluoje <kobomarun@gmail.com>
2018-04-16 00:37:40 +0100

    exit app code

--- www/js/profile.js
+++ www/js/profile.js
@@ -4,3 +4,4 @@ document.getElementById('fname').innerHTML = localStorage.getItem('fname');
 $('#lname').html(localStorage.getItem('lname'));
 $('#id').html(localStorage.getItem('id'));
 $('#phone').html(localStorage.getItem('phone'));
+$('#bname').html(localStorage.getItem('bname'));
